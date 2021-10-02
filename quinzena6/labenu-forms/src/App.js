5c0759b8cf8cad4841cc8c89350929aec4ed932d
midoriobana <ingridmidori@live.com>
2021-10-02 00:54:16 -0300

    criando os componentes e as etapas

--- quinzena6/labenu-forms/src/App.js
+++ quinzena6/labenu-forms/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Etapa1 from './components/Etapa1';
+import Etapa2 from './components/Etapa2';
+import Etapa3 from './components/Etapa3';
+import Final from './components/Final';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Etapa1 />
+        <Etapa2 />
+        <Etapa3 />
+        <Final />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ quinzena6/labenu-forms/src/components/Etapa1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Etapa1 extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>ETAPA 1 - DADOS GERAIS</h2>
+                <p>1. Qual o seu nome?</p>
+                <p>2. Qual a sua idade?</p>
+                <p>3. Qual o seu email?</p>
+                <p>4. Qual a sua escolaridade?</p>
+            </div>
+        )
+    }
+}
+
+export default Etapa1;
